33fb18667f5f860b20324d0eaf5c337aa663ab20
umedzhan <umedzhan@kali.kali>
2025-03-26 12:49:10 +0500

    fix dashboard Lilies Link

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { ReactComponent as HomeIcon } from "../assets/icons/home-icon.svg";
 import { ReactComponent as ProfileIcon } from "../assets/icons/profile-icon.svg";
 import { ReactComponent as OrdersIcon } from "../assets/icons/orders-icon.svg";
@@ -9,10 +10,10 @@ function SideBar() {
 	return (
 		<div className="fixed left-0 h-full max-w-[320px] w-[320px] bg-[#fbfbfb]">
 			<div className="flex flex-col h-full gap-[112px]">
-				<div className="flex gap-[19px] mt-[126px] justify-center">
+				<Link to="/" className="flex gap-[19px] mt-[126px] justify-center">
 					<img className="w-[57.47px] h-[67px]" src="lilies-logo.png" />
 					<h1 className="text-[36px] text-4xl font-bold pt-[19px]">Lilies</h1>
-				</div>
+				</Link>
 				<div className="flex flex-col gap-[15px] justify-center px-[25px]">
 					<div
 						className={`rounded flex gap-[19px] items-center px-[15px] py-[13px] hover:bg-[#cacaca]
